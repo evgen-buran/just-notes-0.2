83f2d9a6abfedd783248025ab74e62238cb139b0
evgen-buran <evgen-buran@yandex.ru>
2023-07-13 19:20:13 +0500

    5th video finished in MainActivity set binding navController toolbar

--- app/src/main/java/com/example/justnotes02/MainActivity.kt
+++ app/src/main/java/com/example/justnotes02/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.justnotes02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.Navigation
+import com.example.justnotes02.databinding.ActivityMainBinding
+import com.example.justnotes02.utils.APP_ACTIVITY
 
+//
 class MainActivity : AppCompatActivity() {
+    lateinit var toolbar: Toolbar
+    lateinit var navController: NavController
+    private var _binding: ActivityMainBinding? = null
+    val binding: ActivityMainBinding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        toolbar = binding.toolbar
+        APP_ACTIVITY = this
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setSupportActionBar(toolbar)
+        toolbar.title = getString(R.string.title)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/example/justnotes02/utils/constants.kt
@@ -0,0 +1,5 @@
+package com.example.justnotes02.utils
+
+import com.example.justnotes02.MainActivity
+
+lateinit var APP_ACTIVITY: MainActivity
